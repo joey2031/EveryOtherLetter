6308c6c6a80d0dc75082496fa89c6bf46a8bac51
joey2031 <36198917+joey2031@users.noreply.github.com>
2018-04-15 10:11:51 -0400

    Update EveryOtherLetter.java

--- EveryOtherLetter/src/everyotherletter/EveryOtherLetter.java
+++ EveryOtherLetter/src/everyotherletter/EveryOtherLetter.java
@@ -10,7 +10,7 @@ public class EveryOtherLetter {
     public static void main(String[] args) {
        
 
-        String word = ("hello"); // j(0) e(2)
+        String word = ("hello"); // h(0) o(4)
         
         for (int i = 0; i < word.length(); i = i + 2) { // o + 2 = 2, 2 + 2 = 4, 4 + 2 = 6 ....
             // FIRST  i becomes 2 and keeps adding the new answer.
